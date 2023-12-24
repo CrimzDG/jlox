a04c497ae92256d3b68f01c7cd517a3db3b12b3b
GEORGE MIHAI MICAN <george.mican.2022@mumail.ie>
2023-12-24 00:27:06 +0000

    Add runFile, runPrompt and run method

--- lox/Lox.java
+++ lox/Lox.java
@@ -4,17 +4,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox{
     public static void main(String[] args) throws IOException {
-	if(args.length > 1) {
+	if (args.length > 1) {
 	    System.out.println("Usage: jlox [script]");
 	    System.exit(64);
 	}
 	
-	else if(args.length == 1) {
+	else if (args.length == 1) {
 	    runFile(args[0]);
 	}
 	
@@ -22,6 +22,32 @@ public class Lox{
 	    runPrompt();
 	}
 	
+    }
+
+    public static void runFile(String path) throws IOException {
+	byte[] bytes = Files.readAllBytes(Paths.get(path));
+	run(new String(bytes, Charset.defaultCharset()));
+    }
+
+    public static void runPrompt() throws IOException {
+	InputStreamReader input = new InputStreamReader(System.in);
+	BufferedReader reader = new BufferedReader(input);
+
+	for (;;) {
+	    System.out.print("> ");
+	    String line = reader.readLine();
+	    if (line == null) break;
+	    run(line);
+	}
+    }
+
+    public static void run(String source) {
+	Scanner scanner = new Scanner(source);
+	List<Token> tokens = scanner.scanTokens();
+
+	for (Token token : tokens) {
+	    System.out.println(token);
+	}
     }
 }
 

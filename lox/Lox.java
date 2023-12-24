7c1161ac0fcb85b011e554ca09474fd000d7c259
GEORGE MIHAI MICAN <george.mican.2022@mumail.ie>
2023-12-24 00:55:12 +0000

    Add error and report method

--- lox/Lox.java
+++ lox/Lox.java
@@ -8,6 +8,8 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox{
+    static boolean hadError = false;
+    
     public static void main(String[] args) throws IOException {
 	if (args.length > 1) {
 	    System.out.println("Usage: jlox [script]");
@@ -27,6 +29,9 @@ public class Lox{
     public static void runFile(String path) throws IOException {
 	byte[] bytes = Files.readAllBytes(Paths.get(path));
 	run(new String(bytes, Charset.defaultCharset()));
+
+	//Exit code indicates error here
+	if (hadError) System.exit(65);
     }
 
     public static void runPrompt() throws IOException {
@@ -38,6 +43,7 @@ public class Lox{
 	    String line = reader.readLine();
 	    if (line == null) break;
 	    run(line);
+	    hadError = false;
 	}
     }
 
@@ -48,6 +54,15 @@ public class Lox{
 	for (Token token : tokens) {
 	    System.out.println(token);
 	}
+    }
+
+    static void error(int line, String message) {
+	report(line, "", message); 
+    }
+
+    private static void report(int line, String where, String message) {
+	System.err.println("[line " + line + "] Error" + where + ": " + message);
+	hadError = true;
     }
 }
 

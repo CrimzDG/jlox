970fa59e1d9d354ac91fa82a361620f853e039a2
GEORGE MIHAI MICAN <george.mican.2022@mumail.ie>
2023-12-26 12:51:20 +0000

    Do rpn problem, similar to my college AST Assignment

--- lox/AstPrinter.java
+++ lox/AstPrinter.java
@@ -7,12 +7,12 @@ class AstPrinter implements Expr.Visitor<String> {
 
     @Override
     public String visitBinaryExpr(Expr.Binary expr) {
-	return parenthesize(expr.operator.lexeme, expr.left, expr.right);
+	return expr.left.accept(this) + " " + expr.right.accept(this) + " " + expr.operator.lexeme;
     }
 
     @Override
     public String visitGroupingExpr(Expr.Grouping expr) {
-	return parenthesize("group", expr.expression);
+	return expr.expression.accept(this);
     }
 
     @Override
@@ -23,30 +23,22 @@ class AstPrinter implements Expr.Visitor<String> {
 
     @Override
     public String visitUnaryExpr(Expr.Unary expr) {
-	return parenthesize(expr.operator.lexeme, expr.right);
-    }
-
-    private String parenthesize(String name, Expr... exprs) {
-	StringBuilder builder = new StringBuilder();
-
-	builder.append("(").append(name);
-	for (Expr expr: exprs) {
-	    builder.append(" ");
-	    builder.append(expr.accept(this));
-	}
-	builder.append(")");
-
-	return builder.toString();
+	return expr.right.accept(this) + " " + expr.operator;
     }
 
     public static void main(String[] args) {
         Expr expression = new Expr.Binary(
-					  new Expr.Unary(
-							 new Token(TokenType.MINUS, "-", null, 1),
-							 new Expr.Literal(123)),
-					  new Token(TokenType.STAR, "*", null, 1),
-					  new Expr.Grouping(
-							    new Expr.Literal(45.67)));
+					  new Expr.Binary(
+							  new Expr.Literal(1),
+							  new Token(TokenType.PLUS, "+", null, 1),
+							  new Expr.Literal(2)),
+					  new Token(TokenType.PLUS, "*", null, 1),
+					  new Expr.Binary(
+							  new Expr.Literal(4),
+							  new Token(TokenType.MINUS, "-", null, 1),
+							  new Expr.Literal(3)
+							  ));
+						
 	
         System.out.println(new AstPrinter().print(expression));
     }
